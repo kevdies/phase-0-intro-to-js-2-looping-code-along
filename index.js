7f09afd1dbebd2299e3523dbe0b3a6741b1cd3d8
kevin diesenberg <kdiesenb@gmail.com>
2022-11-01 11:36:06 -0400

    commit tree 3cc957c65388ddd1fe3d10882642a9042fc7ce7d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+const writeCards = (array, eventName) => {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+const countDown = int => {
+    let count = int;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
